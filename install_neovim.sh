0b0a7abf997428ad1c90bf8755336a77d462fefd
benf <benjamin.frazer.1997@gmail.com>
2025-07-15 15:42:24 +0000

    nvim version updated to 0.11.2

--- install_neovim.sh
+++ install_neovim.sh
@@ -2,9 +2,9 @@
 
 set -e # exit on errors
 
-#rm -rf $HOME/local/neovim/
-#git clone -b v0.10.4 https://github.com/neovim/neovim.git $HOME/local/neovim 
-#sudo apt install cmake gettext lua5.1 liblua5.1-0-dev
+rm -rf $HOME/local/neovim/
+git clone -b v0.11.2 https://github.com/neovim/neovim.git $HOME/local/neovim 
+sudo apt install cmake gettext lua5.1 liblua5.1-0-dev
 cd $HOME/local/neovim
 make CMAKE_BUILD_TYPE=Release
 make CMAKE_INSTALL_PREFIX=$HOME/local install
