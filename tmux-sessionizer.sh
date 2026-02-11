857ef8137a437b8cedf94698ce28818ca9f79743
benjamin frazer <benjamin.frazer@cabridgeaerospace.com>
2026-02-11 17:34:53 +0000

    TMUX Sessioniser: support .workspace files and git work trees

--- tmux-sessionizer.sh
+++ tmux-sessionizer.sh
@@ -18,9 +18,10 @@ else
         "$HOME/dotfiles/.config/nvim"
     )
     
-    # Find git repos in search paths and combine with specific folders
+    # Find git repos and .workspace-marked directories, combine with specific folders
     selected=$(
-        (find "${search_paths[@]}" -mindepth 2 -maxdepth 2 -type d -name ".git" -exec dirname {} \; 2>/dev/null
+        (find "${search_paths[@]}" -mindepth 2 -maxdepth 2 -name ".git" -exec dirname {} \; 2>/dev/null
+         find "${search_paths[@]}" -maxdepth 5 -name ".workspace" -exec dirname {} \; 2>/dev/null
          printf '%s\n' "${specific_folders[@]}" 2>/dev/null) | sort -u | fzf
     )
 fi
@@ -32,13 +33,11 @@ fi
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
-    exit 0
-fi
-
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
     tmux new-session -ds $selected_name -c $selected
+    if [[ -f "$selected/.workspace" ]]; then
+        session_name=$selected_name session_dir=$selected source "$selected/.workspace"
+    fi
 fi
 
 if [[ -z $TMUX ]]; then
